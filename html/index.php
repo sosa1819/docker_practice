9ebcceb1520f9dc651ec7697a9b4b0dbc92fa6ab
sosa1819 <chibanaoya001@gmail.com>
2019-08-06 13:57:12 +0900

    first commit of

---
+++ html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ソサのホームページ</title>
+</head>
+<body>
+ <header>
+     <h1>ソサのホームページ</h1>
+ </header>
+</body>
+</html>
