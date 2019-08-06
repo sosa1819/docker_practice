574dd6846c19128f464a61c0e0348f650b3fd329
sosa1819 <chibanaoya001@gmail.com>
2019-08-06 15:30:26 +0900

    add contact message

--- html/index.php
+++ html/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,17 +8,23 @@
     <link rel="stylesheet" href="/styles/index.css">
     <title>ソサのホームページ</title>
 </head>
+
 <body>
- <header>
-     <h1>ソサのホームページ</h1>
- </header>
- <section><img src="/images/profile.png" alt="自画像">
-<ul>
-    <li>千葉直矢</li>
-    <li>生年月日　1994年5月10日</li>
-    <li>血液型　b型</li>
-    <li><a href="#">プロフィール</a></li>
-</ul>
-</section>
+    <header>
+        <h1>ソサのホームページ</h1>
+    </header>
+    <section><img src="/images/profile.png" alt="自画像">
+        <ul>
+            <li>千葉直矢</li>
+            <li>生年月日　1994年5月10日</li>
+            <li>血液型　b型</li>
+            <li><a href="#">プロフィール</a></li>
+        </ul>
+    <section class="contact-message">
+    　<span> If you have any enquiries regarding my TV drama or film, or would like to make an enquiry concerning future projects, please do not hesitate to contact me through the following email address.
+        <div>mail: <a href="mailto:chibanaoya001@gamil.com">chibanaoya001@gmail.com</a></div>
+
+
 </body>
+
 </html>
