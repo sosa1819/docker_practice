e45f19ec2701119762400a90d954b89f54b70a74
sosa1819 <chibanaoya001@gmail.com>
2019-08-06 14:19:41 +0900

    自画像追加

--- html/index.php
+++ html/index.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/styles/index.css">
     <title>ソサのホームページ</title>
 </head>
 <body>
  <header>
      <h1>ソサのホームページ</h1>
  </header>
+ <section><img src="/images/profile.png" alt="自画像"></section>
 </body>
 </html>
